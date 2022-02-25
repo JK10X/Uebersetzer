b5b0b532c7cdab49cf0d67c76f343b066c49c17b
Jakob Leisering <mailfuerjkaob@gmail.com>
2022-02-25 15:28:49 +0100

    test

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("hallo")
+from urllib2 import urlopen as urlopen
+
+response = urllib2.request('https://raw.githubusercontent.com/JK10X/Uebersetzer/main/to-do.txt')
+data = response.read()
+print(data)
