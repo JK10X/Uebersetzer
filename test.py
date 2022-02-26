2a8ffabe5c6421f5a80ca87a1af756da3a0d19bb
Jakob Leisering <mailfuerjkaob@gmail.com>
2022-02-26 01:35:13 +0100

    urllib test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from urllib2 import urlopen as urlopen
+import urllib
+from urllib import request
 
-response = urllib2.request('https://raw.githubusercontent.com/JK10X/Uebersetzer/main/to-do.txt')
+response = request.urlopen('https://raw.githubusercontent.com/JK10X/Uebersetzer/main/to-do.txt')
 data = response.read()
+data = data.decode()
+ 
 print(data)
